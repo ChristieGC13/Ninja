f341c1e8905a80137f9de1541e5e0662c3254488
Christie Caiano <christiecaiano@gmail.com>
2021-05-04 13:25:32 -0400

    first commit

---
+++ ninja.js
@@ -0,0 +1,23 @@
+class Ninja  {
+    constructor (name, health, speed = 3, strength = 2) {
+        this.name = name;
+        this.health = health;
+        this.speed = speed;
+        this.strength = strength;
+    }
+
+    sayName() {
+        console.log(`My name is ${this.name}`);
+    } 
+
+    showStats() {
+        console.log(`Name: ${this.name}, Strength: ${this.strength}, Speed: ${this.speed}, Health: ${this.health}`);
+    }
+
+    drinkSake() {
+        this.health += 10;
+    }
+
+
+
+}
